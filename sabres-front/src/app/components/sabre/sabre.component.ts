5d5a4a0456b3ab34fdecda8654d17e6a930c5692
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2024-03-21 07:08:42 -0300

    Iniciando o front do projeto

---
+++ sabres-front/src/app/components/sabre/sabre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sabre',
+  standalone: true,
+  imports: [],
+  templateUrl: './sabre.component.html',
+  styleUrl: './sabre.component.css'
+})
+export class SabreComponent {
+
+}

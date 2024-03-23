5946777b7658657fb68bdb29a1cf053108883a71
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2024-03-22 21:46:09 -0300

    Criacao da configuracao do proxy para conexao com o back

--- sabres-front/src/app/sabres/services/sabres.service.ts
+++ sabres-front/src/app/sabres/services/sabres.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SabresService {
 
-  private readonly API = 'http://localhost:8080/';
+  private readonly API = 'api/sabres';
 
   constructor(private httpClient: HttpClient) { }
 
